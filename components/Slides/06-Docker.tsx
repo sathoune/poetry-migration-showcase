e86049873f32f3439c81782806b6b11277962189
sathoune <michal5bargiel@gmail.com>
2022-11-28 00:51:21 +0100

    ✨ Mention docker template.

--- components/Slides/06-Docker.tsx
+++ components/Slides/06-Docker.tsx
@@ -2,6 +2,10 @@ const Docker = () => {
   return (
     <section>
       <h2>Docker integration</h2>
+      <a href="https://github.com/sathoune/python-poetry-docker-template">
+        Template starter project
+      </a>
+      <div>Made with cookiecutter</div>
     </section>
   );
 };
