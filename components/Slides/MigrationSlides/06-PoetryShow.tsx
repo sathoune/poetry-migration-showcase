be4cdeef9085a8255a84811af5674c11d4bdfa39
sathoune <michal5bargiel@gmail.com>
2022-11-27 22:27:02 +0100

    ✨ Tree view.

--- components/Slides/MigrationSlides/06-PoetryShow.tsx
+++ components/Slides/MigrationSlides/06-PoetryShow.tsx
@@ -1,7 +1,11 @@
 const PoetryShow = () => {
   return (
     <section>
-      <h2>Run `poetry show -t/--tree`</h2>
+      <h2>Find secondary dependencies</h2>
+      <pre>
+        <code>poetry show -t/--tree</code>
+      </pre>
+      <img src="/images/sniffio.gif" alt={"Search for duplicated packages."} />
     </section>
   );
 };
