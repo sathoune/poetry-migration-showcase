4f0dcc4362f208a31e07070ad0ea1d59b9aca7dd
sathoune <michal5bargiel@gmail.com>
2022-11-27 22:05:04 +0100

    ✨ PipenvToPoetry.

--- components/Slides/MigrationSlides/01-PipenvToPoetry.tsx
+++ components/Slides/MigrationSlides/01-PipenvToPoetry.tsx
@@ -1,7 +1,15 @@
 const PipenvToPoetry = () => {
   return (
     <section>
-      <h2>Pipenv to Poetry migrate</h2>
+      <h2>Pipenv to Poetry</h2>
+      <a href={"https://github.com/yhino/pipenv-poetry-migrate"}>
+        Script by yhino
+      </a>
+
+      <img
+        src={"/images/pipenv-to-poetry.png"}
+        alt={"GitHub page screenshot"}
+      />
     </section>
   );
 };
