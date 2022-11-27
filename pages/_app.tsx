3d88c94dfd290b85cdc601f77df8b0b218df48c0
sathoune <michal5bargiel@gmail.com>
2022-11-27 19:58:37 +0100

    🔥 Import css from node modules.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import "../public/reveal.css";
-import "../public/theme/moon.css";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/moon.css";
+import "reveal.js/dist/reset.css";
+import "reveal.js/plugin/highlight/monokai.css";
+
 import "../styles/globals.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
