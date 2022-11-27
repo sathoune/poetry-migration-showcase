b888db59f4d0789dd87a73e6f5762dd5f5edafdb
sathoune <michal5bargiel@gmail.com>
2022-11-27 23:02:05 +0100

    ✨ Pin adjustment.

--- components/Slides/MigrationSlides/09-AdjustPins.tsx
+++ components/Slides/MigrationSlides/09-AdjustPins.tsx
@@ -1,7 +1,33 @@
+const pinnedDependencies = `[tool.poetry.dependencies]
+python = "^3.11"
+asyncpg = "==0.27.0"
+fastapi = "==0.88.0"
+pandas = "==1.5.2"
+redis = "==4.3.5"
+sqlalchemy = "==1.4.44"
+uvicorn = "==0.20.0"`;
+
+const unPinnedDependencies = `[tool.poetry.dependencies]
+python = "^3.11"
+asyncpg = "^0.27.0"
+fastapi = "^0.88.0"
+pandas = "^1.5.2"
+redis = "^4.3.5"
+sqlalchemy = "^1.4.44"
+uvicorn = "^0.20.0"`;
+
 const AdjustPins = () => {
   return (
     <section>
       <h2>Adjust pinned dependencies</h2>
+      <div className={"r-stack"}>
+        <pre className={"fragment fade-out"}>
+          <code>{pinnedDependencies}</code>
+        </pre>
+        <pre className={"fragment current-visible"}>
+          <code>{unPinnedDependencies}</code>
+        </pre>
+      </div>
     </section>
   );
 };
