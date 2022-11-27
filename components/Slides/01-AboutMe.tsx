94da4b177c7c0d9a49118cde1d8f341bec75a64e
sathoune <michal5bargiel@gmail.com>
2022-11-28 00:40:32 +0100

    ✨ Contact information.

--- components/Slides/01-AboutMe.tsx
+++ components/Slides/01-AboutMe.tsx
@@ -1,7 +1,31 @@
 const AboutMe = () => {
   return (
     <section>
-      <h2>About me</h2>
+      <div>
+        <h2>About me</h2>
+        <div>
+          <h4>Find me</h4>
+          <ul>
+            <li>
+              <a href="https://github.com/sathoune">@github</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/michal-bargiel/">
+                @linkedin
+              </a>
+            </li>
+            <li>
+              <a href="https://bargiel.dev">@bargiel.dev</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>Presentation available at:</h4>
+          <ul>
+            <li>TBD</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
