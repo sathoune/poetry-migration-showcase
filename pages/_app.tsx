6a1ea8943545dd1f8f877a8ca89c7a19c5caccbd
sathoune <michal5bargiel@gmail.com>
2022-11-28 23:49:53 +0100

    💄 Import remote css.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/moon.css";
 import "reveal.js/dist/reset.css";
 import "reveal.js/plugin/highlight/monokai.css";
-
+import "../styles/remote.css";
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
