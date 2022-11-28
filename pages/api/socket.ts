c6d8000e3a9d9998ad2b042a8e0ccba012f52ccf
sathoune <michal5bargiel@gmail.com>
2022-11-28 23:01:03 +0100

    ♻️ Fix websocket endpoint.

---
+++ pages/api/socket.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+//
+// type Data = {
+//   name: string
+// }
+//
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+import { Server } from "Socket.IO";
+import { initConnection } from "./socketHandlers";
+import { v4 } from "uuid";
+
+// @ts-ignore
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+  } else {
+    console.log("Socket is initializing");
+    const io = new Server(res.socket.server, { path: "/api/socket" });
+    res.socket.server.io = io;
+    io.on("connection", (socket) => initConnection(socket, "/", v4(), null));
+  }
+  res.end();
+};
+
+export default SocketHandler;
