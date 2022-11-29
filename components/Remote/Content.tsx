24079e088274e1c28bb942d42ff62e2ec0679f3e
sathoune <michal5bargiel@gmail.com>
2022-11-29 01:05:16 +0100

    🐛 Fix badly refactored urls.

--- components/Remote/Content.tsx
+++ components/Remote/Content.tsx
@@ -1,18 +1,5 @@
 // @ts-ignore
-// import slideControl from "./scripts";
-const slideControl = {
-  showMenu: () => {},
-  up: () => {},
-  next: () => {},
-  left: () => {},
-  right: () => {},
-  prev: () => {},
-  down: () => {},
-  pause: () => {},
-  overview: () => {},
-  autoslide: () => {},
-  hideMenu: () => {},
-};
+import slideControl from "./scripts";
 
 const Content = () => {
   return (
@@ -23,96 +10,52 @@ const Content = () => {
         <div id="progress"></div>
       </div>
 
-      <a
-        className="show-button"
-        href="components/Remote/Content#"
-        onClick={slideControl.showMenu}
-      >
+      <a className="show-button" href="#" onClick={slideControl.showMenu}>
         <img src="menu.svg" />
       </a>
 
       <div className="buttons">
         <div className="arrows">
           <span>&nbsp;</span>
-          <a
-            href="components/Remote/Content#"
-            id="up"
-            onClick={slideControl.up}
-          >
+          <a href="#" id="up" onClick={slideControl.up}>
             <img src="arrow.svg" style={{ transform: "rotate(0.5turn)" }} />
           </a>
           <span>&nbsp;</span>
-          <a
-            href="components/Remote/Content#"
-            id="next"
-            onClick={slideControl.next}
-          >
+          <a href="#" id="next" onClick={slideControl.next}>
             <img src="next.svg" />
           </a>
 
-          <a
-            href="components/Remote/Content#"
-            id="left"
-            onClick={slideControl.left}
-          >
+          <a href="#" id="left" onClick={slideControl.left}>
             <img src="arrow.svg" style={{ transform: "rotate(0.25turn)" }} />
           </a>
           <span>&nbsp;</span>
-          <a
-            href="components/Remote/Content#"
-            id="right"
-            onClick={slideControl.right}
-          >
+          <a href="#" id="right" onClick={slideControl.right}>
             <img src="arrow.svg" style={{ transform: "rotate(0.75turn)" }} />
           </a>
-          <a
-            href="components/Remote/Content#"
-            id="prev"
-            onClick={slideControl.prev}
-          >
+          <a href="#" id="prev" onClick={slideControl.prev}>
             <img src="next.svg" style={{ rotate: "180deg" }} />
           </a>
 
           <span>&nbsp;</span>
-          <a
-            href="components/Remote/Content#"
-            id="down"
-            onClick={slideControl.down}
-          >
+          <a href="#" id="down" onClick={slideControl.down}>
             <img src="arrow.svg" />
           </a>
           <span>&nbsp;</span>
-          <a
-            href="components/Remote/Content#"
-            id="pause"
-            onClick={slideControl.pause}
-          >
+          <a href="#" id="pause" onClick={slideControl.pause}>
             <img src="off-on.svg" className="hidden-active" />
             <img src="off.svg" className="shown-active" />
           </a>
 
-          <a
-            href="components/Remote/Content#"
-            id="overview"
-            onClick={slideControl.overview}
-          >
+          <a href="#" id="overview" onClick={slideControl.overview}>
             <img src="browse-on.svg" className="hidden-active" />
             <img src="browse.svg" className="shown-active" />
           </a>
-          <a
-            href="components/Remote/Content#"
-            id="autoslide"
-            onClick={slideControl.autoslide}
-          >
+          <a href="#" id="autoslide" onClick={slideControl.autoslide}>
             <img src="play-on.svg" className="hidden-active" />
             <img src="play.svg" className="shown-active" />
           </a>
           <span>&nbsp;</span>
-          <a
-            href="components/Remote/Content#"
-            className="hide-button"
-            onClick={slideControl.hideMenu}
-          >
+          <a href="#" className="hide-button" onClick={slideControl.hideMenu}>
             <img src="menu.svg" />
           </a>
         </div>
@@ -120,7 +63,7 @@ const Content = () => {
         <div className="copyright">
           <span>
             Icons are{" "}
-            <a href="components/Remote/Content" target="_blank">
+            <a href="" target="_blank">
               CC BY
             </a>{" "}
             from{" "}
